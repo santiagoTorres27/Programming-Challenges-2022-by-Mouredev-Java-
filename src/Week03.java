0f6c5c619a22ec312bb1f7567bc2fd3716dd5f70
santiagoTorres27 <santiago.torres27@outlook.com>
2023-11-27 18:25:01 +0100

    week 03: is prime number

---
+++ src/Week03.java
@@ -0,0 +1,21 @@
+public class Week03 {
+
+    /*
+     * Escribe un programa que se encargue de comprobar si un número es o no primo.
+     * Hecho esto, imprime los números primos entre 1 y 100.
+     */
+
+    public static void main(String[] args) {
+        int number = 17;
+        System.out.println(isPrimeNumber(number));
+    }
+
+    public static boolean isPrimeNumber(int number) {
+        for (int i = 2; i < number/2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
